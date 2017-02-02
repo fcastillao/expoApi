63a7a8e7f6e053e61855362913e190856c2ba929
Fernando Castilla <fcastillao@psl.com.co>
2017-02-01 20:04:53 -0500

    sonarqube dijo que una clase tenia minuscula

--- src/main/java/com/example/PokeController.java
+++ src/main/java/com/example/PokeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class pokeController {
+public class PokeController {
 	ArrayList<Pokemon> pokes = new ArrayList<>();
 
 	@RequestMapping("/pokemon")
