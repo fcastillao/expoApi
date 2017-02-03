fce0f3b2078389d9756a92b25b5322c621329a46
fcastillao <fcastillao@psl.com.co>
2017-02-03 15:06:33 -0500

    added pokemon Repository

---
+++ src/main/java/com/example/PokemonRepository.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PokemonRepository extends CrudRepository<Pokemon, Long> {
+
+    List<Pokemon> findById(int id);
+}
