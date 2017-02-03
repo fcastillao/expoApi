daa441a42af6fbee054f08ba806fe100c561388f
fcastillao <fcastillao@psl.com.co>
2017-02-03 16:46:40 -0500

    manejo de repositorio

--- src/main/java/com/example/PokemonDaoRepository.java
+++ src/main/java/com/example/PokemonDaoRepository.java
@@ -5,84 +5,109 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PokemonDaoRepository implements PokemonDao {
 
+	@Autowired
+	PokemonRepository pokemonRepository;
+
+	@Autowired
+	TypeRepository typeRepository;
+
 	@PostConstruct
-	private ArrayList<Type> llenarTipos() {
-		ArrayList<Type> tiposLocales = new ArrayList<Type>();
+	private void llenarTipos() {
 
 		Type electric = new Type();
 		electric.setType("electric");
+		typeRepository.saveAndFlush(electric);
 
 		Type normal = new Type();
 		normal.setType("normal");
+		typeRepository.saveAndFlush(normal);
 
 		Type fire = new Type();
 		fire.setType("fire");
+		typeRepository.saveAndFlush(fire);
 
 		Type fighting = new Type();
 		fighting.setType("fighting");
+		typeRepository.saveAndFlush(fighting);
 
 		Type water = new Type();
 		water.setType("water");
+		typeRepository.saveAndFlush(water);
 
 		Type flying = new Type();
 		flying.setType("flying");
+		typeRepository.saveAndFlush(flying);
 
 		Type grass = new Type();
 		grass.setType("grass");
+		typeRepository.saveAndFlush(grass);
 
 		Type poison = new Type();
 		poison.setType("poison");
+		typeRepository.saveAndFlush(poison);
 
 		Type ground = new Type();
 		ground.setType("ground");
+		typeRepository.saveAndFlush(ground);
 
 		Type psychic = new Type();
 		psychic.setType("spychic");
+		typeRepository.saveAndFlush(psychic);
 
 		Type rock = new Type();
 		rock.setType("rock");
+		typeRepository.saveAndFlush(rock);
 
 		Type ice = new Type();
 		ice.setType("ice");
+		typeRepository.saveAndFlush(ice);
 
 		Type bug = new Type();
 		bug.setType("bug");
+		typeRepository.saveAndFlush(bug);
 
 		Type dragon = new Type();
 		dragon.setType("dragon");
+		typeRepository.saveAndFlush(dragon);
 
 		Type ghost = new Type();
 		ghost.setType("ghost");
+		typeRepository.saveAndFlush(ghost);
 
 		Type dark = new Type();
 		dark.setType("dark");
+		typeRepository.saveAndFlush(dark);
 
 		Type steel = new Type();
 		steel.setType("steel");
+		typeRepository.saveAndFlush(steel);
 
 		Type fairy = new Type();
 		fairy.setType("fairy");
-
-		return tiposLocales;
+		typeRepository.saveAndFlush(fairy);
+		
+		
 
 	}
 
 	@Override
-	public ArrayList<Type> getTipos() {
-		ArrayList<Type> todosLosTipos = new ArrayList<Type>();
-		todosLosTipos = llenarTipos();
+	public List<Type> getTipos() {
+		List<Type> todosLosTipos = new ArrayList<Type>();
+		todosLosTipos = typeRepository.findAll();
+
 		return todosLosTipos;
 	}
 
 	@Override
-	public ArrayList<Type> getTipos(int id) {
-		ArrayList<Type> todosLosTipos = new ArrayList<Type>();
-		todosLosTipos = llenarTipos();
+	public List<Type> getTipos(int id) {
+		List<Type> todosLosTipos = new ArrayList<Type>();
+		todosLosTipos = typeRepository.findAll();
 
 		if (id == 0) {
 			return todosLosTipos;
@@ -100,8 +125,8 @@ public class PokemonDaoRepository implements PokemonDao {
 
 	@Override
 	public ArrayList<Type> getTipos(String typeName) {
-		ArrayList<Type> todosLosTipos = new ArrayList<Type>();
-		todosLosTipos = llenarTipos();
+		List<Type> todosLosTipos;
+		todosLosTipos = typeRepository.findAll();
 
 		for (Type type : todosLosTipos) {
 			if (type.getType().equals(typeName)) {
@@ -115,47 +140,52 @@ public class PokemonDaoRepository implements PokemonDao {
 	}
 
 	@PostConstruct
-	public ArrayList<Pokemon> getPokemonList() {
-		ArrayList<Pokemon> pokemonesLocales = new ArrayList<>();
-		List<Type> tipoElectrico = new ArrayList<Type>();
-		tipoElectrico.add(new Type(1, "electric"));
-		ArrayList<Type> debilidadesElectricas = new ArrayList<Type>();
-		debilidadesElectricas.add(new Type(9, "ground"));
+	public void fillPokemonRepository() {
+		Pokemon raichu = new Pokemon();
+		raichu.setEvolution(null);
+		raichu.setImage("TODO");
+		raichu.setTipos(typeRepository.findByType("electric"));
+		raichu.setDebilidades(typeRepository.findByType("ground"));
+		raichu.setNombre("raichu");
+		
+		Pokemon pikachu = new Pokemon();
+		raichu.setEvolution(raichu);
+		raichu.setImage("http://www.dondevive.org/wp-content/uploads/2016/07/pikachu.jpg");
+		raichu.setTipos(typeRepository.findByType("electric"));
+		raichu.setDebilidades(typeRepository.findByType("ground"));
+		raichu.setNombre("pikachu");
 
-		ArrayList<Type> tipoHierba = new ArrayList<Type>();
-		tipoHierba.add(new Type(7, "grass"));
-		ArrayList<Type> debilidadesHierba = new ArrayList<Type>();
-		debilidadesHierba.add(new Type(3, "fire"));
-		debilidadesHierba.add(new Type(12, "ice"));
-		debilidadesHierba.add(new Type(6, "flying"));
-		debilidadesHierba.add(new Type(10, "psychic"));
-
-		Pokemon raichu = new Pokemon(2, "raichu", tipoElectrico, debilidadesElectricas, null, "TODO");
-		pokemonesLocales.add(raichu);
-
-		Pokemon pikachu = new Pokemon(1, "pikachu", tipoElectrico, debilidadesElectricas, raichu,
-				"http://www.dondevive.org/wp-content/uploads/2016/07/pikachu.jpg");
-		pokemonesLocales.add(pikachu);
-
-		Pokemon bulbasaur = new Pokemon(3, "bulbasaur", tipoHierba, debilidadesHierba, null,
-				"http://cartoonbros.com/wp-content/uploads/2016/11/Bulbasaur-8.png");
-		pokemonesLocales.add(bulbasaur);
-		return pokemonesLocales;
-
+		Pokemon bulbasaur = new Pokemon();
+		bulbasaur.setNombre("bulbasaur");
+		bulbasaur.setTipos(typeRepository.findByType("grass"));
+		
+		ArrayList<Type> debilidadesHierba = new ArrayList<>();
+		debilidadesHierba.add(typeRepository.findByType("bug").get(0));
+		debilidadesHierba.add(typeRepository.findByType("fire").get(0));
+		debilidadesHierba.add(typeRepository.findByType("flying").get(0));
+		debilidadesHierba.add(typeRepository.findByType("ice").get(0));
+		debilidadesHierba.add(typeRepository.findByType("poison").get(0));
+		
+		bulbasaur.setDebilidades(debilidadesHierba);
+		bulbasaur.setEvolution(null);
+		bulbasaur.setImage("http://cartoonbros.com/wp-content/uploads/2016/11/Bulbasaur-8.png");
+		pokemonRepository.saveAndFlush(raichu);
+		pokemonRepository.saveAndFlush(pikachu);
+		pokemonRepository.save(bulbasaur);
 	}
 
 	@Override
 	public List<Pokemon> getPokes() {
-		ArrayList<Pokemon> todosLosPokemones = new ArrayList<Pokemon>();
-		todosLosPokemones = getPokemonList();
+		List<Pokemon> todosLosPokemones = new ArrayList<Pokemon>();
+		todosLosPokemones = pokemonRepository.findAll();
 		return todosLosPokemones;
 
 	}
 
 	@Override
 	public ArrayList<Pokemon> getPokes(String name) {
-		ArrayList<Pokemon> todosLosPokes = new ArrayList<Pokemon>();
-		todosLosPokes = getPokemonList();
+		List<Pokemon> todosLosPokes = new ArrayList<Pokemon>();
+		todosLosPokes = pokemonRepository.findAll();
 
 		for (Pokemon pokemon : todosLosPokes) {
 			if (pokemon.getNombre().equals(name)) {
@@ -170,8 +200,8 @@ public class PokemonDaoRepository implements PokemonDao {
 
 	@Override
 	public ArrayList<Pokemon> getPokes(int parseInt) {
-		ArrayList<Pokemon> todosLosPokes = new ArrayList<Pokemon>();
-		todosLosPokes = getPokemonList();
+		List<Pokemon> todosLosPokes = new ArrayList<Pokemon>();
+		todosLosPokes = pokemonRepository.findAll();
 
 		for (Pokemon pokemon : todosLosPokes) {
 			if (pokemon.getId() == parseInt) {
