0bbd5507b76fab2dff794b90eac7a84ba5d01fc9
fcastillao <fcastillao@psl.com.co>
2017-02-03 16:48:23 -0500

    removida la creacion de oikemones del main

--- src/main/java/com/example/PokeApplication.java
+++ src/main/java/com/example/PokeApplication.java
@@ -10,12 +10,7 @@ public class PokeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PokeApplication.class, args);
 
-		Pokemon pikachu = new Pokemon();
-		pikachu.setNombre("pikachu");
-
-		Pokemon raichu = new Pokemon();
-		pikachu.setNombre("raichu");
-		pikachu.setEvolution(raichu);
+		
 
 	}
 }
