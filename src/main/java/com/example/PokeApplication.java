7ce878bcf0cda236d8e3f1f763dce8d797d1db73
fcastillao <fcastillao@psl.com.co>
2017-02-03 15:40:32 -0500

    working on main

--- src/main/java/com/example/PokeApplication.java
+++ src/main/java/com/example/PokeApplication.java
@@ -1,14 +1,28 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
 public class PokeApplication {
+	
+	@Autowired
+	PokemonDao pokemonDao;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokeApplication.class, args);
+		
+		Pokemon pikachu = new Pokemon();
+		pikachu.setNombre("pikachu");
 
+		Pokemon raichu = new Pokemon();
+		pikachu.setNombre("raichu");
+		pikachu.setEvolution(raichu);
+		
+		
+		
 	}
 }
