90f591c67f9ebbba6c818e65e633140fc07bd54a
fcastillao <fcastillao@psl.com.co>
2017-02-06 14:39:06 -0500

    added @Repository

--- src/main/java/com/example/PokemonRepository.java
+++ src/main/java/com/example/PokemonRepository.java
@@ -3,7 +3,8 @@ package com.example;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
 	List<Pokemon> findByPokemonId(int pokemonId);
--- src/main/java/com/example/TypeRepository.java
+++ src/main/java/com/example/TypeRepository.java
@@ -1,7 +1,8 @@
 package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface TypeRepository extends JpaRepository<Type, Long> {
 
 	Type findByTypeName(String typeName);
