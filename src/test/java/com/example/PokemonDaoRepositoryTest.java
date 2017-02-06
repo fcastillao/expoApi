6a340f477e0563c0b6b330787a20334fe7f99031
fcastillao <fcastillao@psl.com.co>
2017-02-06 14:39:22 -0500

    ADDED ONE TEST AAAAAHHHHH

---
+++ src/test/java/com/example/PokemonDaoRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PokemonDaoRepositoryTest{
+	
+	@Autowired
+    private TestEntityManager entityManager;
+	
+	@Autowired
+	PokemonRepository pokemonRepository;
+	
+	@Autowired
+	TypeRepository typeRepository;
+	
+
+	@Test
+	public void savePikachu() {
+		
+		Pokemon pikachu = new Pokemon();
+		pikachu.setNombre("pikachu");
+		Type electric = new Type(1,"electric");
+		Type ground = new Type(2,"ground");
+		ArrayList<Type> grounds = new ArrayList<>();
+		grounds.add(ground);
+		pikachu.setTipos(electric);
+		pikachu.setDebilidades(grounds);
+		pikachu.setImage("TODO");
+		entityManager.persist(pikachu);
+		pikachu.setPokemonId(1);
+		
+		assertEquals(pikachu, pokemonRepository.findByNombre("pikachu"));
+		
+	}
+
+}
