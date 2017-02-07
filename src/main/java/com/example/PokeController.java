4fedb38da3926dc7f72e97fd691dd45733b85b0a
fcastillao <fcastillao@psl.com.co>
2017-02-07 13:52:44 -0500

    removed thymeleaf

--- src/main/java/com/example/PokeController.java
+++ src/main/java/com/example/PokeController.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//TODO ver que paso aqui  
-//import org.thymeleaf.exceptions.ParserInitializationException;
-
 @Controller
 public class PokeController {
 	ArrayList<Pokemon> pokes = new ArrayList<>();
